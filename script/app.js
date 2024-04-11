41d505bc00ee43343346fb137c7c2d9c762996e6
Kyle Oliva <kecoliva@gmail.com>
2024-04-11 17:14:20 -0400

    refactor: fetch product in product details

--- script/app.js
+++ script/app.js
@@ -9,18 +9,6 @@ function filterProducts(category = null, offer = null, id_index = 0) {
   );
 }
 
-function filterProductById(id) {
-  $.ajax({
-    type: "GET",
-    url: "/script/json/product.json",
-    success: function (data) {
-      products = data;
-      let row = products.filter((item) => item.id == id);
-      return row.length == 0 ? null : row[0];
-    },
-  });
-}
-
 function createProductCard(product) {
   let productCard = document.createElement("div");
   productCard.classList.add("product-card");
--- script/product_detail.js
+++ script/product_detail.js
@@ -2,44 +2,49 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 // Get the product ID from the URL
-const productId = urlParams.get('productId');
+const productId = urlParams.get("productId");
 
 // Asynchronous function to add product details to HTML
 // Asynchronous function to add product details to HTML
 const addProductToHtml = async () => {
-  try {
-    // Find the product details based on the product ID (assuming productId is defined somewhere)
-    const product = filterProductById(parseInt(productId));
+  $.ajax({
+    type: "GET",
+    url: "/script/json/product.json",
+    success: function (data) {
+      try {
+        let row = data.filter((item) => item.id == productId);
 
-    if (product !== null) {
-      // Accessing elements by class name
-      const productImages = document.querySelectorAll('.product-image');
-      const thumbnailImages = document.querySelectorAll('.thumbnail-image');
-      productImages.forEach((image, index) => {
-        image.src = product[`image${index + 1}`];
-      });
+        // Find the product details based on the product ID (assuming productId is defined somewhere)
+        const product = row.length == 0 ? null : row[0];
 
-      // Update src attributes of thumbnail images
-      thumbnailImages.forEach((image, index) => {
-        image.src = product[`image${index + 1}`];
-      });
+        if (product !== null) {
+          // Accessing elements by class name
+          const productImages = document.querySelectorAll(".product-image");
+          const thumbnailImages = document.querySelectorAll(".thumbnail-image");
+          productImages.forEach((image, index) => {
+            image.src = product[`image${index + 1}`];
+          });
 
-      $('body').find('.product-title').html(product.name);
-      $('body').find('.product-link').html(product.category);
-    }
-    else {
-      $('.p-detail-card').html('<h2>Oops! We were unavailable to find the product you were looking for.</h2>')
-    }
+          // Update src attributes of thumbnail images
+          thumbnailImages.forEach((image, index) => {
+            image.src = product[`image${index + 1}`];
+          });
 
-
-  } catch (error) {
-    console.error('Error fetching product details: ', error);
-  }
-
+          $("body").find(".product-title").html(product.name);
+          $("body").find(".product-link").html(product.category);
+        } else {
+          $(".p-detail-card").html(
+            "<h2>Oops! We were unavailable to find the product you were looking for.</h2>"
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching product details: ", error);
+      }
+    },
+  });
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Call the function to add product details to HTML
   addProductToHtml();
 });
-
